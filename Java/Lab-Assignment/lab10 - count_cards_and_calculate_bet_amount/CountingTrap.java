fb9cbacc1d880a81c7c4cd44c7dadee7b3143e63
Marist User <techengineervivek@gmail.com>
2018-11-06 15:15:32 -0500

    updated lab10 with apt solution

--- Java/Lab-Assignment/lab10 - count_cards_and_calculate_bet_amount/CountingTrap.java
+++ Java/Lab-Assignment/lab10 - count_cards_and_calculate_bet_amount/CountingTrap.java
@@ -1,7 +1,6 @@
 package charlie.client;
 
 import charlie.card.Card;
-import charlie.card.Hid;
 import charlie.message.Message;
 import charlie.message.view.to.Deal;
 import charlie.plugin.ITrap;
@@ -14,6 +13,11 @@ import charlie.message.view.to.GameStart;
  */
 public class CountingTrap implements ITrap {
     private final Logger LOG = Logger.getLogger(CountingTrap.class);  
+    private int shoesize = 52;
+    private int runningCount = 0;
+    private double numDecks = 0.0;
+    private int trueCount = 0;
+    private int betAmt = 0;    
     
     @Override
     public void onSend(Message msg) {
@@ -21,13 +25,7 @@ public class CountingTrap implements ITrap {
     }
 
     @Override
-    public void onReceive(Message msg) {
-        int shoesize = 0;
-        int runningCount = 0;
-        double numDecks = 0.0;
-        int trueCount = 0;
-        int betAmt = 0;
-        
+    public void onReceive(Message msg) {        
         LOG.info("onReceive overriding function from CountingTrap.java file");
         
         if(msg instanceof GameStart) {
@@ -43,10 +41,12 @@ public class CountingTrap implements ITrap {
             if(card == null) return;
             
             if(card.getRank() == 10 || card.isFace() || card.isAce()) {
-                --runningCount;
+                runningCount -= 1;
+                LOG.info("decrement runningCount: " + runningCount);                
             } else if(card.getRank() <=6) {
-                ++runningCount;
-            }           
+                runningCount += 1;
+                LOG.info("increment runningCount: " + runningCount);                                
+            }                   
             
             numDecks = shoesize / 52.0;
             trueCount = (int) Math.round((runningCount / numDecks));
@@ -58,9 +58,6 @@ public class CountingTrap implements ITrap {
                     + "| trueCount: " + trueCount 
                     + "| bet amount: " + betAmt);
         }
-        
-        // bet_amt = max((1, true_count + 1));
-        // (int)(0.5 + bet_amt)
     }
     
 }
