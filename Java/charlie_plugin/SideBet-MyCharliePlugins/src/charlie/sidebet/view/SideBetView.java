9e3564f95781887238246bd66fba0d9a34b1e8d1
Vivek Vellaiyappan <techengineervivek@gmail.com>
2018-12-11 23:13:11 -0500

    added assignment 4 code

--- Java/charlie_plugin/SideBet-MyCharliePlugins/src/charlie/sidebet/view/SideBetView.java
+++ Java/charlie_plugin/SideBet-MyCharliePlugins/src/charlie/sidebet/view/SideBetView.java
@@ -29,15 +29,16 @@ public class SideBetView implements ISideBetView {
     public final static int X = 400;
     public final static int Y = 200;
     public final static int DIAMETER = 50;
-    
-    //Used for placing chips in random
+   
     protected Random ran = new Random();
-    // Used for rendering
     Graphics2D graphicsObject;
 
     protected Font font = new Font("Arial", Font.BOLD, 18);
     protected Font fontBet = new Font("Arial", Font.BOLD, 12);
     protected BasicStroke stroke = new BasicStroke(3);
+    private double PAYOFF;
+    int width;
+    protected List<charlie.view.sprite.Chip> chips = new ArrayList<>();
 
     // See http://docs.oracle.com/javase/tutorial/2d/geometry/strokeandfill.html
     protected float dash1[] = {10.0f};
@@ -50,12 +51,7 @@ public class SideBetView implements ISideBetView {
     protected List<ChipButton> buttons;
     protected int amt = 0;
     protected AMoneyManager moneyManager;
-    
-    int width;
-    private double PAYOFF;
-    //***
-    protected List<charlie.view.sprite.Chip> chips = new ArrayList<>();
-
+   
     public SideBetView() {
         LOG.info("side bet view constructed");
         moneyManager = new AMoneyManager();
@@ -79,41 +75,20 @@ public class SideBetView implements ISideBetView {
     @Override
     public void click(int x, int y) {
         int oldAmt = amt;
-
+        
         // Test if any chip button has been pressed.
         for(ChipButton button: buttons) {
-            LOG.info("chips button: " + button);
-            if (button.isReady() && button.isPressed(x, y)) {
+            if(button.isPressed(x, y)) {
                 amt += button.getAmt();
-                // For Sound while adding Chips
-                SoundFactory.play(Effect.CHIPS_IN);
-                
-                int n = chips.size();
-                //Add chips with random position
-                width = button.getImage().getWidth(null);
-                int placeX = X + (n) * width / 3 + ran.nextInt(10) - 10;
-                int placeY = Y + ran.nextInt(15) + 15;
-                charlie.view.sprite.Chip chip = 
-                        new charlie.view.sprite.Chip(button.getImage(), 
-                                placeX, placeY, amt);
-                chips.add(chip);
-
-                LOG.info("A. side bet amount "+button.getAmt()+
-                        " updated new amt = "+amt);
-            }
+                LOG.info("A. side bet amount "+button.getAmt()+" updated new amt = "+amt);
+            } 
         }
-
+        
         if(oldAmt == amt) {
-            if (x < (X + DIAMETER / 2) && x > X - (DIAMETER / 2) && 
-                    y > (Y - DIAMETER / 2) && y < (Y + DIAMETER / 2)) {
-                amt = 0;
-                chips.clear();
-                // For Sound while removing Chips
-                SoundFactory.play(Effect.CHIPS_OUT);
-                LOG.info("B. side bet amount cleared");
-            }
+            amt = 0;
+            LOG.info("B. side bet amount cleared");
         }
-    }
+    }    
 
     /**
      * Informs view the game is over and it's time to update the bankroll for the hand.
@@ -128,10 +103,9 @@ public class SideBetView implements ISideBetView {
 
         LOG.info("side bet outcome = "+bet);
 
-        // Update the bankroll
         moneyManager.increase(bet);
         PAYOFF = bet;
-        LOG.info("new bankroll = "+moneyManager.getBankroll());
+        LOG.info("Updated new bankroll = "+ moneyManager.getBankroll());
     }
 
     /**
@@ -193,65 +167,47 @@ public class SideBetView implements ISideBetView {
             Chip chip = chips.get(i);
             chip.render(g);
         }
-
-        //Render the state of the bet
         renderState(graphicsObject);
     }
     
-    /*
-     * This method checks the bet amount and the PAYOFF and
-     * displays whether it is a win or lose for the side bets.
+    /**
      * 
+     * @param g 
      */
     protected void renderState(Graphics2D g) {
         try {
             String result;
 
-            //Check if the PAYOFF is zero and return as no action is needed
             if (PAYOFF == 0) {
                 return;
-            } 
-            
-            //If PAYOFF is higher than or same as bet amount then it is a WIN situation
-            else if (PAYOFF >= amt) {
+            } else if (PAYOFF >= amt) {
                 result = "WIN";
-            } 
-
-            //If PAYOFF is less than bet amount than it is a loss.
-            else {
+            } else {
                 result = "LOSE";
             }
 
-            //Set background color for ovals based on Win or Lose
             if ("LOSE".equals(result)) {
                 g.setColor(new Color(205, 92, 92));
             } else if ("WIN".equals(result)) {
                 g.setColor(new Color(0, 255, 0));
             }
 
-            //Set the font
             Font outcomeFont = new Font("Arial", Font.BOLD, 18);
             FontMetrics fm = g.getFontMetrics(outcomeFont);
             
-            //Form the result text that needs to be displayed.
             String outcomeText = " " + result.toUpperCase() + " ! ";
-            
-            //Get the height and width of the text that will be displayed
+           
             int w = fm.charsWidth(outcomeText.toCharArray(), 0, 
                     outcomeText.length());
             int h = fm.getHeight();
-            
-            //Paint the oval with the background color.
             g.fillRoundRect(X + 10, Y - h + 50, w, h, 5, 5);
 
-            // Paint the outcome foreground
             if ("LOSE".equals(result)) {
                 g.setColor(Color.WHITE);
             } else if ("WIN".equals(result)) {
                 g.setColor(Color.BLACK);
             }
 
-            //Draw the out come text.
             g.setFont(outcomeFont);
             g.drawString(outcomeText, X + 10, Y + 45);
         } 
